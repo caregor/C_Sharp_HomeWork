67653e3f536eae17df41aa54b15e52fa58a78737
caregor <caregor@gmail.com>
2022-06-22 14:31:57 +0300

    The Task 2 is done

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int first_number = 0;
+int second_number = 0;
+
+enter_number(ref first_number);
+enter_number(ref second_number);
+
+if (first_number > second_number) 
+{
+    Console.WriteLine($" max = {first_number}");
+    Console.WriteLine($" min = {second_number}");
+}
+else
+{
+    Console.WriteLine($" max = {second_number}");
+    Console.WriteLine($" min = {first_number}");
+}
+
+
+static void enter_number (ref int num)
+{
+    Console.Write("Enter a number > ");
+    string inputStr = Console.ReadLine();
+    num = int.Parse(inputStr);
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+Console.Write("Enter a 1 number > ");
+int first_number = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a 2 number > ");
+int second_number = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a 3 number > ");
+int third_number = int.Parse(Console.ReadLine());
+
+// condition for 1 number
+if ((first_number > second_number) & (first_number > third_number))
+{
+    Console.WriteLine($"Numbers {first_number}, {second_number}, {third_number} max -> {first_number}");
+}
+
+// condition for 2 number
+if ((first_number < second_number) & (second_number > third_number))
+{
+    Console.WriteLine($"Numbers {first_number}, {second_number}, {third_number} max -> {second_number}");
+}
+
+// condition for 3 number
+if ((third_number > second_number) & (first_number < third_number))
+{
+    Console.WriteLine($"Numbers {first_number}, {second_number}, {third_number} max -> {third_number}");
+}
