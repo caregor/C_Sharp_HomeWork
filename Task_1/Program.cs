418701e0ed3228a6dc7c218f0e1c74dbb9c73204
caregor <caregor@gmail.com>
2022-06-22 13:47:23 +0300

    The Task 1 is done

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,10 +2,24 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-int a = 0;
 
-enter_number(ref a);
-Console.WriteLine(a);
+int first_number = 0;
+int second_number = 0;
+
+enter_number(ref first_number);
+enter_number(ref second_number);
+
+if (first_number > second_number) 
+{
+    Console.WriteLine($" max = {first_number}");
+    Console.WriteLine($" min = {second_number}");
+}
+else
+{
+    Console.WriteLine($" max = {second_number}");
+    Console.WriteLine($" min = {first_number}");
+}
+
 
 static void enter_number (ref int num)
 {
